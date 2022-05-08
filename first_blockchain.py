04c3c2e29cc7353fb61b3f43ba35a50696bcd764
reshav1234 <71163222+reshav1234@users.noreply.github.com>
2022-05-08 19:55:15 +0545

    Add files via upload
    Building Cryptocurrency using python

---
+++ first_blockchain.py
@@ -0,0 +1,55 @@
+# Blue print for first block chain
+import hashlib
+import json 
+from time import time     
+
+
+class blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+        # Creating a genesis block
+        self.new_block(previous_hash = 1, proof = 1000)
+
+
+    def new_block(self, proof, previous_has):
+        # Creates a new block and add it to the chain
+        block = {
+            'index' : len(self.chain) + 1,
+            'timestamp' : time(),
+            'transaction' : self.current_transaction,
+            'proof' : proof,
+            'previous_hash' : self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transaction
+        self.current_transaction = []
+        self.chain.append(block)
+        return block 
+
+    def new_transaction(self):
+        # adds a transaction to the list of trasnaction
+        self.current_transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+    
+
+    @staticmethod
+    def hash(block):
+        
+        # Hashes a block
+        block_string = json.dump(block, sort_keys = True).encode() 
+        """ Storing in dictionary in order:or we will have inconsistent hashes """
+
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        # Returns last block in the chain
+        return self.chain[-1]
+
+
